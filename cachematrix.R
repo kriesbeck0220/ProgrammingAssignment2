dc11306eb3b3af874c4ef7b5e12bbd9824d47ab8
kriesbeck0220 <kevin.william.riesbeck@gmail.com>
2015-05-24 11:51:42 -0500

    for assignment solution submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions, in tandum, calculate the inverse of a given matrix and store the value of that inverse for future recall.
 
-## Write a short comment describing this function
+## begins by setting the inverse to NULL as a placeholder for a future value
+## defines a function to set the matrix, x, to a new matrix y, and resets the inverse, i, to NULL
+## defines function that returns the matrix, x
+## defines function that sets the inverse, i, to a new inverse, inverse
+## defines a function that returns the inverse, i
+## returns a 'special vector' containing all of the functions defined above
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL  
+	set <- function(y) {  
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x  
+	setinverse <- function(inverse) i <<- inverse  
+	getinverse <- function() i  
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
+## retrives the cahced value of the inverse, i, of a matrix defined by the function makeCacheMatrix
+## if the inverse, i, has already been calculated and cached then returns the cahced value
+## if the inverse has not already been cached, then caclulate the inverse and cache this new calculated value for futrure recall
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()  
+	if(!is.null(i)) {  
+		message("getting cached data")
+		return(i)
+	}
+	data <- x$get()  
+	i <- solve(data, ...)
+	x$setinverse(i)
+	i
 }
